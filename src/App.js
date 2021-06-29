a514af1c2278a59ec3ea6f34c348882eda1fa7d3
vladward <vladward13@gmail.com>
2021-06-29 20:44:07 +0300

    Add Navbar and css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import {Switch, Route, Redirect} from "react-router-dom";
 
 const App = () => {
     return (
         <div>
-            <Header />
-            <Navbar />
+            <Switch>
+                <Header/>
+                <Navbar/>
+                <Route path="/profile" component={Profile}/>
+                <Route path="/MyGames" component={MyGames}/>
+                <Route path="/Statistics" component={Statistics}/>
+                <Route path="/Friends" component={Friends}/>
+                <Route exact path='/' render={() => <Redirect to={"/profile"}/>}/>
+            </Switch>
         </div>
     );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,26 @@
 import React from "react"
 import styles from "./Navbar.module.css"
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div></div>
+        <nav className={styles.nav}>
+            <div className={styles.item}>
+                <NavLink to="/profile" activeClassName={styles.active}>My Profile</NavLink>
+            </div>
+            <div className={styles.item}>
+                <a>Music</a>
+            </div>
+            <div className={styles.item}>
+                <a>Music</a>
+            </div>
+            <div className={styles.item}>
+                <a>Music</a>
+            </div>
+            <div className={styles.item}>
+                <a>Music</a>
+            </div>
+        </nav>
     )
 }
 
