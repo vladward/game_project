38f913212bfbe29b7c6092506c003e867e2f05a9
vladward <vladward13@gmail.com>
2021-06-29 19:18:48 +0300

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
 import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
     return (
         <div>
-            <Header/>
+            <Header />
+            <Navbar />
         </div>
     );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+import styles from "./Navbar.module.css"
+
+const Navbar = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Navbar
