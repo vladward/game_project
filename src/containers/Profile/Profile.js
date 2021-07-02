281b442d8718170cb32dc7b75668ef6588d6a8b4
vladward <vladward13@gmail.com>
2021-07-02 23:15:32 +0200

    Добавил grid Добавил список игр в Profile page

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -1,13 +1,18 @@
 import React from "react"
 import styles from './Profile.module.css'
 import userPhoto from "./../../images/user_profile.png"
+import cod from "./../../images/gameIcons/cod.png"
+import csgo from "./../../images/gameIcons/csgo.png"
+import dota from "./../../images/gameIcons/dota.png"
+import fortnite from "./../../images/gameIcons/fortnite.png"
+import minecraft from "./../../images/gameIcons/minecraft.png"
 
 const Profile = () => {
     return (
         <div className={styles.item}>
             <div className={styles.container}>
                 <div className={styles.ava}>
-                    <img src={userPhoto} alt=""/>
+                    <img src={userPhoto} alt=""/><br/>
                     <button>Subscribe</button>
                 </div>
                 <div className={styles.name}>
@@ -23,6 +28,30 @@ const Profile = () => {
                         <em>email:</em> <a>vladward@gmail.com</a></label>--user.email<br/>
                     <label><i className="fas fa-gamepad"></i>&nbsp;
                         <em>like a:</em> <a>Adventure, Action, Party, Survival</a></label>--user.likes<br/>
+                </div>
+                <div className={styles.games}>
+                    <div className={styles.games_Container}>
+                        <div className={styles.game_item}>
+                            <img src={cod}/><br/>
+                            <i>Call of Duty</i>
+                        </div>
+                        <div className={styles.game_item}>
+                            <img src={csgo}/><br/>
+                            <i>CS:GO</i>
+                        </div>
+                        <div className={styles.game_item}>
+                            <img src={dota}/><br/>
+                            <i>Dota 2</i>
+                        </div>
+                        <div className={styles.game_item}>
+                            <img src={fortnite}/><br/>
+                            <i>Fortnite</i>
+                        </div>
+                        {/*<div className={styles.game_item}>*/}
+                        {/*    <img src={minecraft}/>*/}
+                        {/*    <i>Minecraft</i>*/}
+                        {/*</div>*/}
+                    </div>
                 </div>
             </div>
         </div>
